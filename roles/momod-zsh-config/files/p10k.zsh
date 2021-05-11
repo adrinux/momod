33d1607d62bde2177c3a763209d39393a0e65aba
Adrian Simmons <adrian@perlucida.co.uk>
2021-05-11 13:17:41 +0100

    Use Ubuntu brand colours

--- roles/momod-zsh-config/files/p10k.zsh
+++ roles/momod-zsh-config/files/p10k.zsh
@@ -172,7 +172,7 @@
 
   #################################[ os_icon: os identifier ]##################################
   # OS identifier color.
-  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=30
+  typeset -g POWERLEVEL9K_OS_ICON_FOREGROUND=202
   typeset -g POWERLEVEL9K_OS_ICON_BACKGROUND=7
   # Custom icon.
   # typeset -g POWERLEVEL9K_OS_ICON_CONTENT_EXPANSION='⭐'
@@ -202,7 +202,7 @@
 
   ##################################[ dir: current directory ]##################################
   # Current directory background color.
-  typeset -g POWERLEVEL9K_DIR_BACKGROUND=30
+  typeset -g POWERLEVEL9K_DIR_BACKGROUND=53
   # Default current directory foreground color.
   typeset -g POWERLEVEL9K_DIR_FOREGROUND=254
   # If directory is too long, shorten some of its segments to the shortest possible unique
